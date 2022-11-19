e006ae719815e020db08939f652ac70b3bf5da8d
GoodmanBen <goodmanben@gmail.com>
2022-11-18 20:24:55 -0500

    fix: type in output description

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "api_url" {
   value       = google_cloud_run_service.https_job_trigger.status[0].url
-  description = "The url where requests for the https dragondrop trigger hosted on GCP Cloud Run can be sent."
+  description = "The url where requests to the https dragondrop trigger hosted on GCP Cloud Run can be sent."
 }
