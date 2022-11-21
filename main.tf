d9d5f84b50af4f8f301b5af722081e3aaeffed06
GoodmanBen <goodmanben@gmail.com>
2022-11-20 20:01:02 -0500

    fix: add project field to custom iam role

--- main.tf
+++ main.tf
@@ -1,18 +1,19 @@
 # Ensuring that the cloud run api is enabled.
 resource "google_project_service" "sql_admin_api" {
-  service = "run.googleapis.com"
-  project = var.project
+  service            = "run.googleapis.com"
+  project            = var.project
   disable_on_destroy = false
 }
 
 resource "google_service_account" "cloud_run_service_account" {
-  account_id = var.service_account_name
+  account_id  = var.service_account_name
   description = "Service accounts used by the Cloud Run Services and Jobs that host the dragondrop.cloud service."
-  project = var.project
+  project     = var.project
 }
 
 // Custom role for the service account, the minimum amount needed to update and invoke jobs
 resource "google_project_iam_custom_role" "dragondrop-https-trigger-role" {
+  project     = var.project
   role_id     = "dragondropHTTPSTriggerRole"
   title       = "dragondrop HTTPS Trigger Role"
   description = "Role for the dragondrop https trigger to update and invoke Cloud Run Jobs."
@@ -21,14 +22,14 @@ resource "google_project_iam_custom_role" "dragondrop-https-trigger-role" {
 
 resource "google_project_iam_member" "cloud_run_invoker" {
   project = var.project
-  role   = google_project_iam_custom_role.dragondrop-https-trigger-role.id
-  member = "serviceAccount:${google_service_account.cloud_run_service_account.email}"
+  role    = google_project_iam_custom_role.dragondrop-https-trigger-role.id
+  member  = "serviceAccount:${google_service_account.cloud_run_service_account.email}"
 }
 
 resource "google_cloud_run_service" "https_job_trigger" {
-    name = var.https_trigger_cloud_run_service_name
-    location = var.region
-    project = var.project
+    name                       = var.https_trigger_cloud_run_service_name
+    location                   = var.region
+    project                    = var.project
     autogenerate_revision_name = true
 
     template {
@@ -43,12 +44,12 @@ resource "google_cloud_run_service" "https_job_trigger" {
                image = "us-east4-docker.pkg.dev/dragondrop-dev/dragondrop-https-triggers/cloud-run-service:latest"
 
                env {
-                 name = "JOB_NAME"
+                 name  = "JOB_NAME"
                  value = var.dragondrop_engine_cloud_run_job_name
                }
 
                env {
-                 name = "JOB_REGION"
+                 name  = "JOB_REGION"
                  value = var.region
                }
 
@@ -61,12 +62,12 @@ resource "google_cloud_run_service" "https_job_trigger" {
                    // these resource specifications should be more than sufficient.
                    requests = {
                       memory = "400M"
-                      cpu = "1000m"
+                      cpu    = "1000m"
                    }
 
                    limits = {
                      memory = "1000M"
-                     cpu = "2000m"
+                     cpu    = "2000m"
                    }
                }
 
