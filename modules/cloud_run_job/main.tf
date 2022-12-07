41e105e4f5872b1e32d886ceb15f93c3ff1037b2
GoodmanBen <goodmanben@gmail.com>
2022-12-07 11:15:25 -0500

    build: test exact doc cloud run job

--- modules/cloud_run_job/main.tf
+++ modules/cloud_run_job/main.tf
@@ -14,40 +14,70 @@ module "division_to_provider" {
   dragondrop_compute_service_account = var.dragondrop_compute_service_account_email
 }
 
-## Defining the secrets needed for Environment variables of the Cloud Run Job
-resource "google_cloud_run_v2_job" "dragondrop-engine" {
-   provider = google-beta
-   name = var.cloud_run_job_name
-   location = var.region
-   project = var.project
-   launch_stage = "BETA"
+resource "google_cloud_run_v2_job" "default" {
+  name     = "cloudrun-job"
+  location = "us-central1"
+  launch_stage = "BETA"
+  provider = google
 
-   template {
-      template {
-        service_account_name = "serviceAccount:${var.dragondrop_compute_service_account_email}"
-
-        containers {
-          // TODO: change this to a variable name etc.
-          image = "us-docker.pkg.dev/cloudrun/container/hello"
-
-          env {
-            name = module.division_to_provider.secret_id
-            value_source {
-              secret_key_ref {
-                name = module.division_to_provider.secret_id
-                key  = "latest"
-              }
-            }
-          }
-
-          resources {
-            requests = {
-              memory = "1600M"
-              cpu = "4000m"
-            }
-          }
-        }
+  template {
+    template {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
       }
     }
+  }
 }
 
+resource "google_cloud_run_v2_job" "default" {
+  name     = "cloudrun-job"
+  location = "us-central1"
+  launch_stage = "BETA"
+  provider = google-beta
+
+  template {
+    template {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+}
+
+## Defining the secrets needed for Environment variables of the Cloud Run Job
+#resource "google_cloud_run_v2_job" "dragondrop-engine" {
+#   provider = google-beta
+#   name = var.cloud_run_job_name
+#   location = var.region
+#   project = var.project
+#   launch_stage = "BETA"
+#
+#   template {
+#      template {
+#        service_account_name = "serviceAccount:${var.dragondrop_compute_service_account_email}"
+#
+#        containers {
+#          // TODO: change this to a variable name etc.
+#          image = "us-docker.pkg.dev/cloudrun/container/hello"
+#
+#          env {
+#            name = module.division_to_provider.secret_id
+#            value_source {
+#              secret_key_ref {
+#                name = module.division_to_provider.secret_id
+#                key  = "latest"
+#              }
+#            }
+#          }
+#
+#          resources {
+#            requests = {
+#              memory = "1600M"
+#              cpu = "4000m"
+#            }
+#          }
+#        }
+#      }
+#    }
+#}
+#
--- versions.tf
+++ versions.tf
@@ -6,5 +6,10 @@ terraform {
       source  = "hashicorp/google-beta"
       version = ">=4.45.0"
     }
+
+     google = {
+      source  = "hashicorp/google"
+      version = ">=4.45.0"
+    }
   }
 }
